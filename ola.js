3938e79dd8e53382dfaae7c1d5c0c4571698c84b
Guilherme Z <133171632+guilhermezan42@users.noreply.github.com>
2024-10-10 17:41:56 +0000

    exercicio de JS final feito

--- ola.js
+++ ola.js
@@ -3,11 +3,49 @@ const inputNum = document.querySelector("#num");
 const button = document.querySelector("button");
 const button2 = document.querySelector("#btnfim");
 const select = document.querySelector("select");
-function poeNoSelect() {
-  let num = Number(inputNum.value);
-  let opt = document.createElement("option");
-  opt.innerText = num;
-  select.appendChild(opt);
-  inputNum.value = "";
+const main = document.querySelector("main");
+let vetor = [];
+const txt = document.createElement("div");
+function validNum(l) {
+  if (l.value.length !== 0 && !isNaN(l.value)) {
+    return true;
+  }
 }
+function poeNoSelect() {
+  if (!validNum(inputNum)) {
+    window.alert("Digite um numero");
+  } else {
+    let num = Number(inputNum.value);
+    vetor.push(num);
+    let opt = document.createElement("option");
+    opt.innerText = `valor ${num} adicionado`;
+    select.appendChild(opt);
+    inputNum.value = "";
+  }
+}
+function operacoesVetor() {
+  if (select.options.length == 0) {
+    window.alert("Nenhuma lista adicionada");
+  } else {
+    txt.innerHTML = `<p>a lista tem ${vetor.length} numeros</p>`;
+    vetor.sort((a, b) => a - b);
+    txt.innerHTML += `<p>o menor numero é: ${vetor[0]}</p>`;
+    txt.innerHTML += `<p>o maior numero é: ${vetor[vetor.length - 1]}</p>`;
+    let soma = 0;
+    for (let i = 0; i < vetor.length; i++) {
+      soma += vetor[i];
+    }
+    txt.innerHTML += `<p>a soma dos numeros é ${soma} </p>`;
+    txt.innerHTML += `<p>a média ponderada dos numeros é ${
+      soma / vetor.length
+    } </p>`;
+    txt.innerHTML += "<p> " + vetor.join(", ") + "</p>";
+    main.appendChild(txt);
+  }
+}
+
 button.addEventListener("click", poeNoSelect);
+button2.addEventListener("click", operacoesVetor);
+// ao clicar no botao 2 mostrar o tamanho do vetor o menor valor e o maior valor
+// a soma e a média
+// 1° preciso colocar os dados em um array
