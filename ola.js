88ac3c42759342d059ec4a572cd0b9e709d13dfb
Guilherme Z <133171632+guilhermezan42@users.noreply.github.com>
2024-10-10 01:21:37 +0000

    codigo em catalao

--- ola.js
+++ ola.js
@@ -1,1 +1,13 @@
-console.log("olá")
+// <!-- um select que cria opções com os dados e coloca eles em um vetor e depois faz operações com os dados do array -->
+const inputNum = document.querySelector("#num");
+const button = document.querySelector("button");
+const button2 = document.querySelector("#btnfim");
+const select = document.querySelector("select");
+function poeNoSelect() {
+  let num = Number(inputNum.value);
+  let opt = document.createElement("option");
+  opt.innerText = num;
+  select.appendChild(opt);
+  inputNum.value = "";
+}
+button.addEventListener("click", poeNoSelect);
